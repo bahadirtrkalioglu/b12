9fb7f5b4828cb262bff60322796f72b15625ab65
Bahadır Tarık Alioğlu <105515680+bahadirtrkalioglu@users.noreply.github.com>
2023-03-21 18:32:27 +0300

    Added Splash Screen and App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:b12/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-
 void main() async{
-
-  // init hive
-  //await Hive.initFlutter();
-
-  //var box = await Hive.openBox("thebox");
-
 
   runApp(const MyApp());
 }
